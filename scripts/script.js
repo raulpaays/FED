44cfe3107a43ae0f9c5478e795df96625e1cadce
Raúl Perez <112827931+raulpaays@users.noreply.github.com>
2022-10-06 01:02:20 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,12 @@ var hamburgerButton = document.querySelector("nav>a");
 var nav = document.querySelector ("nav ul");
 var kruisButton = document.querySelector ("nav li:first-of-type a");
 
+var herewego = new Audio();
+var mushroom = new Audio();
+
+herewego.src ="./audio/marioherewego.mp3";
+mushroom.src ="./audio/mariomushroom.mp3";
+
 
 hamburgerButton.addEventListener("click", function() { 
     nav.classList.toggle("open"); /* Voegt class toe om te openen */
@@ -15,7 +21,6 @@ kruisButton.addEventListener("click", function() {
     nav.classList.toggle("open"); /* Voegt class toe om te openen */
 
 }, false) /* zodat je er niet doorheen kan klikken */
-
 
 
 
